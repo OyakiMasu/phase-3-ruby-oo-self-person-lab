6d32be8738074afa6ec0709597eebc6345cf5270
OyakMasu21 <akzaki17@gmail.com>
2023-02-12 14:18:43 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -59,6 +59,55 @@ class Person
       @hygiene = 8
     end
   
+    
+   def clean?
+    hygiene > 7
+   end
+   
+    def happy?
+      happiness > 7
+    end
+     
+    def get_paid(amount)
+      self.bank_account += amount
+     "all about the benjamins"
+    end
+
+    def take_bath
+      self.hygiene += 4
+     "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out 
+      self.happiness += 2
+      self.hygiene -= 3
+     "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+      self.happiness += 3
+      friend.happiness += 3
+     "Hi #{friend.name}! It's #{name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+      case topic 
+      when "politics"
+        self.happiness -= 2
+        friend.happiness -= 2
+       "blah blah partisan blah lobbyist"
+      
+      when "weather"
+        self.happiness += 1
+        friend.happiness += 1
+       "blah blah sun blah rain"
+
+      else 
+        "blah blah blah blah blah"
+      end
+    end
+
+
     def happiness=(value)
       @happiness = [0, [value, 10].min].max
     end
@@ -66,9 +115,9 @@ class Person
     def hygiene=(value)
       @hygiene = [0, [value, 10].min].max
     end
+
   end
-    
-  
 
 p = Person.new("Patrick")
+p2 = Person.new("Jesse")
 binding.pry
